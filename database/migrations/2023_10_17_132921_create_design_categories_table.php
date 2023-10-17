987a05d32b62fc5082ab4682acc8d1697c838eaa
Ikhsandi15 <sandiikhsandic@gmail.com>
2023-10-17 22:15:16 +0700

    create table design_categories

---
+++ database/migrations/2023_10_17_132921_create_design_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('design_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('gambar');
+            $table->string('gambar2')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('design_categories');
+    }
+};
