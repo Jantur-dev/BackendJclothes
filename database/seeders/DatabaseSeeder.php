f81e43d73e4bf2d937d29e8b4be01695d0741ed7
Ikhsandi15 <sandiikhsandic@gmail.com>
2023-10-17 22:15:53 +0700

    add call DesignCategoriesSeeder::class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            BarangSeeder::class
+            BarangSeeder::class,
+            DesignCategoriesSeeder::class
         ]);
     }
 }
